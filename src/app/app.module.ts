768c7dd6e46e55313d2feaa32795c1d61a96c96b
Delache <delache33@gmail.com>
2019-10-30 16:31:51 +0100

    feat: monsterComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MovieComponent } from './movie/movie.component';
+import { MonsterComponent } from './monster/monster.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    MonsterComponent
   ],
   imports: [
     BrowserModule,
