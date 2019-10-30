663799e0f264b0d6370d7c2e66ca4137d757a00c
Antoine Moignet <antoinemoignet@gmail.com>
2019-10-30 20:01:57 +0100

    route pour listMovies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { MonsterChoiceComponent } from './pages/monster-choice/monster-choice.co
 import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListMoviesComponent } from './pages/fightPage/list-movies/list-movies.component';
+
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'monsterChoice', component: MonsterChoiceComponent },
+  { path: 'listMovies', component: ListMoviesComponent }
+
 ];
 
 @NgModule({
