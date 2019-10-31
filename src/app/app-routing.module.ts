ca57f2e6aa94ecc300b7c25e50dce9fffb6f9a6d
Antoine Moignet <antoinemoignet@gmail.com>
2019-10-31 08:40:35 +0100

    creation component finalScore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavBarPlayerComponent } from './nav-bar-player/nav-bar-player.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MonsterChoiceComponent } from './pages/monster-choice/monster-choice.component';
+import { FinalscoreComponent } from './pages/finalscore/finalscore.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'monsterChoice', component: MonsterChoiceComponent },
   { path: 'moviesList', component: MoviesListComponent },
   { path: 'navBarPlayer', component: NavBarPlayerComponent },
+  { path: 'finalScore', component: FinalscoreComponent }
 ];
 
 @NgModule({
