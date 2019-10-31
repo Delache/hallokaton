0c09e801014259b29f30e69561d06f3b0342155e
Delache <delache33@gmail.com>
2019-10-31 05:21:55 +0100

    merci hugo

--- src/app/pages/movies-list/movies-list.component.ts
+++ src/app/pages/movies-list/movies-list.component.ts
@@ -1,33 +1,47 @@
-import { RecupMonstersService } from "../../services/recup-monsters.service";
-import { Component, OnInit } from "@angular/core";
+import { RecupMonstersService } from '../../services/recup-monsters.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-movies-list",
-  templateUrl: "./movies-list.component.html",
-  styleUrls: ["./movies-list.component.scss"]
+  selector: 'app-movies-list',
+  templateUrl: './movies-list.component.html',
+  styleUrls: ['./movies-list.component.scss']
 })
 export class MoviesListComponent implements OnInit {
   result: {};
   posterUrl: string;
+  randID1: number;
+  randID2: number;
+
+  decks: any[][] = [[{},{},{}],[{},{},{}]];
 
   constructor(private service: RecupMonstersService) {}
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  array = [];
-  askToAPI() {
-    const randID = Math.floor(Math.random() * 82) + 1;
-    this.service.getById(randID).subscribe((maCarteRandom: any) => {
-      this.result = maCarteRandom.movie;
-      this.array.push(this.result);
 
-      return this.array;
-    });
+  // askToAPI1() {
+  //   const randID1 = Math.floor(Math.random() * 82) + 1;
+  //   this.service.getById(randID1).subscribe((maCarteRandom: any) => {
+  //     this.result = maCarteRandom.movie;
+  //   });
+  // }
+  // askToAPI2() {
+  //   const randID1 = Math.floor(Math.random() * 82) + 1;
+  //   this.service.getById(randID1).subscribe((maCarteRandom: any) => {
+  //     this.result = maCarteRandom.movie;
+  // });
+  // }
+
+  askToAPI(i: number, j: number){
+    const randID1 = Math.floor(Math.random() * 82) + 1;
+    this.service.getById(randID1).subscribe((maCarteRandom: any) => {
+      this.decks[i][j] = maCarteRandom.movie;
+  });
   }
-
+}
   // this.service.getAllMovies()
   // .subscribe((response: any)=> {
   //   this.result = response.movies
   //   console.log(this.result)
   // })
-}
+
